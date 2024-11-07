ee90ace2a52938a10661fea2e6c515448a8d8766
Nirmal Kumar <nirmalkumardurai@gmail.com>
2024-11-07 09:55:54 +0530

    todo item create

--- cypress/e2e/todo/todo.cy.js
+++ cypress/e2e/todo/todo.cy.js
@@ -4,41 +4,82 @@ import { text } from "../../page/todo";
 const todo = require('../../page/todo')
 describe('launching TODO website', () => {
 
+  const url = 'https://todomvc.com/examples/react/dist/'
+  let newitem;
+
   beforeEach(() => {
-    cy.visit('https://todomvc.com/examples/react/dist/')
+    cy.visit(url)
+    cy.fixture('tododata').as('todolistdata')
   })
 
-  it('Verify The URL ', () => {
-    cy.url().should('include', "https://todomvc.com/examples/react/dist/")
+  it('should allow a user to Verify The URL ', () => {
+    cy.url().should('include', url)
 
   })
-  const newitem = ["welcome", "apple", "headphone"]
 
-  it('Add new todo items', () => {
-    newitem.forEach(value => {
-      todo.addtodo(value)
-    })
-    todo.gettodocount().then(count => {
-      const getvalueinlist=parseInt(count.match(/\d+/)[0],10);
-      const arraycount=newitem.length
-      expect(arraycount).to.equal(arraycount)
-    })
+  //  const newitem = ["welcome", "apple", "headphone"]
 
+  it('should allow a user to Add new todo items', () => {
+    cy.get('@todolistdata').then((data) => {
+
+
+      data.newitem.forEach(value => {
+        todo.addtodo(value)
+      })
+      todo.gettodocount().then(count => {
+        const getvalueinlist = parseInt(count.match(/\d+/)[0], 10);
+        const arraycount = data.newitem.length
+        expect(getvalueinlist).to.equal(arraycount)
+      })
+    })
 
   })
-  it('should allow a user to edit a to-do item', () => {
-    newitem.forEach(value => {
-      todo.addtodo(value)
+
+  it('should allow a user to edit a todo item', () => {
+    cy.get('@todolistdata').then((data) => {
+      data.newitem.forEach(value => {
+        todo.addtodo(value)
+      })
+      todo.editTodoItem('welcome', 'newcar');
+      todo.getTodoItems().should('contain.text', 'newcar');
     })
-    todo.editTodoItem('welcome', 'newcar');
-    todo.getTodoItems().should('contain.text', 'newcar');
   });
-  it('should allow a user to delete a to-do item', () => {
-    const deletename = "newjoin"
-    newitem.forEach(value => {
-      todo.addtodo(value)
+
+  it('should allow a user to delete a todo item', () => {
+    cy.get('@todolistdata').then((data) => {
+      const deletename = "welcome"
+      data.newitem.forEach(value => {
+        todo.addtodo(value)
+      })
+      todo.deleteItem(deletename);
+      todo.getTodoItems().should('not.contain.text', deletename);
     })
-    todo.deleteItem(deletename);
-    todo.getTodoItems().should('not.contain.text', deletename);
   });
+
+  it('should mark a to-do as completed', () => {
+    todo.addtodo('Complete');
+    todo.markTodoCompleted('Complete');
+    todo.getTodoItems()
+      .contains('Complete')
+      .parents('li')
+      .should('have.class', 'completed');
+  });
+
+  it('should filter active and completed to-dos', () => {
+    cy.get('@todolistdata').then((data) => {
+      const complite = "welcome"
+      data.newitem.forEach(value => {
+        todo.addtodo(value)
+      })
+      todo.markTodoCompleted(complite);
+      todo.filterActiveItems();
+      todo.gettodocount().then(count => {
+      const getvalueinlist = parseInt(count.match(/\d+/)[0], 10);
+      const arraycount = data.newitem.length-1
+      todo.getTodoItems().should('have.length', arraycount);
+      todo.filterCompletedItems();
+      todo.getTodoItems().should('have.length',1);
+    });
+  })
+})
 })
--- cypress/page/todo.js
+++ cypress/page/todo.js
@@ -7,6 +7,7 @@ class todo {
         this.clearCompletedButton = '.clear-completed';
         this.filterActive = 'a[href="#/active"]';
         this.filterCompleted = 'a[href="#/completed"]';
+        this.rediobutton = "//label[contains(text(),'new')]/preceding-sibling::input"
     }
 
     addtodo(entertext) {
@@ -32,8 +33,26 @@ class todo {
     }
     deleteItem(item) {
         cy.contains(this.todoItems, item)
+            .trigger('mouseover')
             .find('.destroy')
             .click({ force: true });
+        // cy.xpath("//label[contains(text(),"+item+")]/preceding-sibling::input[@type='checkbox']") ////label[contains(text(),'welcome')]/preceding-sibling::input[@type='checkbox']
+        // .check()
+        // .trigger('mousemove')
+        // .find('.destroy')
+        // .click({ force: true });
     }
+    markTodoCompleted(item) {
+        cy.contains(this.todoItems, item)
+            .find('.toggle')
+            .check();
+    }
+    filterActiveItems() {
+        cy.get(this.filterActive).click();
+      }
+    
+      filterCompletedItems() {
+        cy.get(this.filterCompleted).click();
+      }
 }
 module.exports = new todo()
